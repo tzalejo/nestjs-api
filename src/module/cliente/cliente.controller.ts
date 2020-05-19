d43f574cce0180ed081d6ca7ed1593f4d0124764
tzalejo <tzalejo@gmail.com>
2020-05-18 22:26:41 -0300

    metodo para cliente

--- src/module/cliente/cliente.controller.ts
+++ src/module/cliente/cliente.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, ParseIntPipe, Param, Patch, Body, Post, Delete } from '@nestjs/common';
+import { ClienteService } from './cliente.service';
+import { Cliente } from './cliente.entity';
 
 @Controller('cliente')
-export class ClienteController {}
+export class ClienteController {
+  constructor(
+    private readonly _clienteService: ClienteService,
+  ){}
+  
+  @Get(':clienteId')
+  get(
+    @Param('clienteId', ParseIntPipe) clienteId: number 
+  ): Promise<Cliente> {
+    return this._clienteService.get(clienteId);
+  }
+
+  @Get()
+  getAll(): Promise<Cliente[]> {
+    return this._clienteService.getAll();
+  }
+
+  @Post()
+  creearCliente(
+    @Body() cliente: Cliente
+  ): Promise<Cliente> {
+    return this._clienteService.crear(cliente);
+  }
+
+  @Patch(':clienteId')
+  updatecliente(
+    @Param('clienteId', ParseIntPipe) clienteId: number,
+    @Body() cliente: Cliente
+  ): Promise<Cliente>{
+    return this._clienteService.update(clienteId, cliente);
+  }
+
+  @Delete(':clienteId')
+  deleteCliente(
+    @Param('clienteId', ParseIntPipe) clienteId: number,
+  ){
+    return this._clienteService.delete(clienteId);
+  }
+}
--- src/module/cliente/cliente.service.ts
+++ src/module/cliente/cliente.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import { ClienteRepository } from './cliente.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cliente } from './cliente.entity';
 
 @Injectable()
-export class ClienteService {}
+export class ClienteService {
+  constructor(
+    @InjectRepository(ClienteRepository)
+    private readonly _clienteRepository: ClienteRepository,
+  ){}
+
+  async get(clienteId: number): Promise<Cliente>{
+    if(!clienteId) throw new BadRequestException('El cliente es necesario');
+    const cliente = await this._clienteRepository.findOne(clienteId);
+    if(!cliente) throw new NotFoundException('No existe el cliente');
+    return cliente;
+  }
+
+  async getAll(): Promise<Cliente[]> {
+    const clientes = await (await this._clienteRepository.find()).sort();
+    if(!clientes) throw new NotFoundException('No existen clientes');
+    return clientes;
+  }
+
+  async crear(cliente: Cliente): Promise<Cliente> {
+    if(!cliente) throw new BadRequestException('El cliente es necesario');
+    const clienteNuevo = await this._clienteRepository.save(cliente);
+    return clienteNuevo;
+  }
+
+  async update(clienteId: number, cliente: Cliente): Promise<Cliente> {
+    if(!clienteId) throw new BadRequestException('El cliente es necesario');
+    const clienteUpdate = await this._clienteRepository.findOne(clienteId);
+    if(!clienteUpdate) throw new NotFoundException('No existe el cliente');
+    // actualizo
+    clienteUpdate.DNI = cliente.DNI;
+    clienteUpdate.nombre = cliente.nombre;
+    clienteUpdate.apellido = cliente.apellido;
+    clienteUpdate.telefono = cliente.telefono;
+    clienteUpdate.email = cliente.email;
+    // guardo y retorno..
+    return await this._clienteRepository.save(clienteUpdate);
+  }
+
+  async delete(clienteId: number): Promise<void>{
+    if(!clienteId) throw new BadRequestException('Es necesario el cliente');
+    // busco el cliente
+    const clienteDelete = await this._clienteRepository.findOne(clienteId);
+    // veo si el cliente tiene formularios.
+    if(!clienteDelete.formularios) throw new NotFoundException('El cliente tiene formularios');
+    // elimino cliente si no tiene formulario
+    await this._clienteRepository.delete(clienteId);
+  }
+
+}
