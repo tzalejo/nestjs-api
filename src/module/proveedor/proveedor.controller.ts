6f2e142af5f783794ae622879c79b02cefc1526d
tzalejo <tzalejo@gmail.com>
2020-05-18 22:26:55 -0300

    metodo para proveedor

--- src/module/proveedor/proveedor.controller.ts
+++ src/module/proveedor/proveedor.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Body, Patch, Delete } from '@nestjs/common';
+import { ProveedorService } from './proveedor.service';
+import { Proveedor } from './proveedor.entity';
 
 @Controller('proveedor')
-export class ProveedorController {}
+export class ProveedorController {
+  constructor(
+    private readonly _proveedorService: ProveedorService,
+  ){}
+
+  @Get(':proveedorId')
+  getProveedor(
+    @Param('proveedorId', ParseIntPipe) proveedorId: number
+  ): Promise<Proveedor>{
+    return this._proveedorService.get(proveedorId);
+  }
+
+  @Get()
+  getProveedores(): Promise<Proveedor[]>{
+    return this._proveedorService.getAll();
+  }
+  
+  @Post()
+  crearProveedor(
+    @Body() proveedor: Proveedor
+  ): Promise<Proveedor>{
+    return this._proveedorService.crear(proveedor);
+  }
+
+  @Patch(':proveedorId')
+  updateProveedor(
+    @Param('proveedorId', ParseIntPipe) proveedorId: number,
+    @Body() proveedor: Proveedor
+  ): Promise<Proveedor>{
+    return this._proveedorService.update(proveedorId, proveedor);
+  }
+
+  @Delete(':proveedorId')
+  deleteProveedor(
+    @Param('proveedorId', ParseIntPipe) proveedorId: number
+  ){
+    return this._proveedorService.delete(proveedorId);
+  }
+}
--- src/module/proveedor/proveedor.service.ts
+++ src/module/proveedor/proveedor.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, BadRequestException, NotFoundException } from '@nestjs/common';
+import { ProveedorRepository } from './proveedor.repository';
+import { Proveedor } from './proveedor.entity';
 @Injectable()
-export class ProveedorService {}
+export class ProveedorService {
+  constructor(
+    private readonly _proveedorRepository: ProveedorRepository,
+  ) {}
+
+  async get(proveedorId): Promise<Proveedor>{
+    // verifico el proveeid q exista
+    if (!proveedorId) throw new BadRequestException('Es necesario el proveedor');
+    // busco el proveedor
+    const proveedor = await this._proveedorRepository.findOne(proveedorId);
+    // devuelvo exceptcion de qu no existe prove
+    if (!proveedor) throw new NotFoundException('El proveedor no existe');
+    return proveedor;
+  }
+
+  async getAll(): Promise<Proveedor[]>{
+    const proveedores = await this._proveedorRepository.find();
+    if(!proveedores) throw new NotFoundException('Proveedor no existe');
+    return proveedores;
+  }
+  async crear(proveedor: Proveedor): Promise<Proveedor>{
+    if(!proveedor) throw new BadRequestException('Es necesario los datos del proveedor');
+    const proveedorNuevo = await this._proveedorRepository.save(proveedor);
+    return proveedorNuevo;
+  }
+
+  async update(proveedorId: number, proveedor: Proveedor): Promise<Proveedor>{
+    if(!proveedorId) throw new BadRequestException('Es necesario el proveedor');
+    const proveeExiste = await this._proveedorRepository.findOne(proveedorId);
+    if(!proveeExiste) throw new NotFoundException('El proveedor no existe');
+    // actualizo
+    proveeExiste.DNI = proveedor.DNI;
+    proveeExiste.nombre = proveedor.nombre;
+    proveeExiste.apellido = proveedor.apellido;
+    proveeExiste.telefono = proveedor.telefono;
+    proveeExiste.email = proveedor.email;
+    // guardo
+    const userUpdate = await this._proveedorRepository.save(proveeExiste);
+    return userUpdate;
+  }
+
+  async delete(proveeId: number): Promise<void> {
+    // veo si viene vacio el elemento a eliminar
+    if(!proveeId) throw new BadRequestException('Es necesario el proveedor');
+    // busco el proveedor
+    const provee = await this._proveedorRepository.findOne(proveeId);
+    // veo si el proveedor tiene formularios.
+    if(!provee.formularios) throw new NotFoundException('El proveedor tiene formularios');
+    // elimino proveedor
+    await this._proveedorRepository.delete(proveeId);
+  }
+  
+}
