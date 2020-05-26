4ddf95b3bfc43441c4efef5f5d37725dc5c7cbf0
tzalejo <tzalejo@gmail.com>
2020-05-26 00:28:05 -0300

    agrego metodo de reset pass

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, ValidationPipe, HttpException, HttpStatus, NotFoundException, Get, Param } from '@nestjs/common';
 import { SignupDto, SigninDto } from './dto';
 import { AuthService } from './auth.service';
 
@@ -12,14 +12,30 @@ export class AuthController {
   @UsePipes(ValidationPipe) // para que cumpla las condiciones de SignupDto
   async signup(
     @Body() signupDto: SignupDto
-  ): Promise<void> {
-    return await this._authService.signup(signupDto);
+  ) {
+    try {
+      await this._authService.signup(signupDto);
+      const emailEnviado = await this._authService.enviarCorreoRegistracion(signupDto.email, signupDto.password);
+      if (emailEnviado) {
+        return true;
+      }
+      throw new HttpException('Registro no se pudo realizar', HttpStatus.FORBIDDEN);
+    } catch (error) {
+      throw new NotFoundException('Error inesperado.', error);
+    }
   }
+
   @Post('/signin')
   @UsePipes(ValidationPipe)
   async signin(
     @Body() signinDto: SigninDto
-  ){
+  ) {
     return await this._authService.signin(signinDto);
   }
+
+  @Get('reset-password/:email')
+  async resetPassUser(@Param() params) {
+    return this._authService.resetPassworUser(params.email);
+  }
+
 }
